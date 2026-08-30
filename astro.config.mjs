15918d1ab276114a3c692b38ac4b5634231c97da
Rodrigo Moncada <104479691+ChocoShit@users.noreply.github.com>
2026-08-30 13:49:07 +0200

    Disable Vercel adapter in astro.config.mjs
    Comment out Vercel adapter configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+//import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
+//  adapter: vercel(),
+  site: 'https://ChocoShit.github.io',
   integrations: [tailwind()],
   server: {
     port: 4321,
